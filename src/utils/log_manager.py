5ef562248fb13b01754c2dd0e44d78dc889d4379
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-01-21 06:15:13 -0300

    fix: fixed logs to show on command docker logs

--- src/utils/log_manager.py
+++ src/utils/log_manager.py
@@ -1,27 +1,27 @@
 import logging
-
+import os
 
 class LogManager:
     loggers = {}
 
     @classmethod
-    def get_logger(cls, name, level=logging.INFO, log_file="app.log"):
+    def get_logger(cls, name, level=logging.INFO, log_file="logs/app.log"):
         if name not in cls.loggers:
             logger = logging.getLogger(name)
             logger.setLevel(level)
+            formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-            )
+            if not logger.handlers:
+                stream_handler = logging.StreamHandler()
+                stream_handler.setFormatter(formatter)
+                logger.addHandler(stream_handler)
 
-            ch = logging.StreamHandler()
-            ch.setFormatter(formatter)
-            logger.addHandler(ch)
+                if log_file:
+                    os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-            if log_file:
-                fh = logging.FileHandler(log_file)
-                fh.setFormatter(formatter)
-                logger.addHandler(fh)
+                    file_handler = logging.FileHandler(log_file)
+                    file_handler.setFormatter(formatter)
+                    logger.addHandler(file_handler)
 
             cls.loggers[name] = logger
 
